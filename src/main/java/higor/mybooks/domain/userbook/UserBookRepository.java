983ba51609d21cf07d7b5629bbbc34fcee02382f
higor-nunes <higorn@dbserver.com.br>
2020-12-06 22:42:48 +0100

    Remove obsolete user book objects.

--- src/main/java/higor/mybooks/domain/userbook/UserBookRepository.java
+++
@@ -1,9 +0,0 @@
-package higor.mybooks.domain.userbook;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface UserBookRepository {
-  Page<UserBook> findByUserId(Integer userId, Pageable pageable);
-  <S extends UserBook> S save(S entity);
-}
